dba3dffb3942d517e79be1ba7d107521da370cdc
BocciGaria <s.otsuka0726@gmail.com>
2024-02-17 16:54:39 +0900

    Initial Commit

---
+++ STROUSTRUPX/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  STROUSTRUPX
+//
+//  Created by 大塚創太 on 2024/02/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
